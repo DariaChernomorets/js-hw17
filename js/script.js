03f85db7d5d1187f02a9a63ce9eaeb2d6d0031ec
Daria <101666130+DariaChernomorets@users.noreply.github.com>
2022-07-24 17:53:15 +0300

    1st try

--- js/script.js
+++ js/script.js
@@ -4,9 +4,8 @@ const num = +prompt('Введите Ваше число');
 const exp = +prompt('Введите степень, в которую хотите возвести число');
 
 const exponentiation = (num,exp = 1) => {
-    if (isNaN(num) || isNaN(exp) || num === 0 || exp === 0 ) return 'some error';
+    if (typeof (num) !== 'number' || typeof(exp) !== 'number'  || isNaN(num) || isNaN(exp)) return 'some error';
     return num**exp;
-
 }
 
 const result = exponentiation(num,exp);
